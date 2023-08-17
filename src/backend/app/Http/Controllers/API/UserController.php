5b555bcde1bbd1b3e4b03944b74fabd7b9eb9daa
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-08-17 15:35:22 +0300

    Updating API documentation

--- src/backend/app/Http/Controllers/API/UserController.php
+++ src/backend/app/Http/Controllers/API/UserController.php
@@ -17,6 +17,41 @@ class UserController
      *     summary="Fetches the users along with their transactions",
      *     operationId="index",
      *     tags={"Users"},
+     *     @OA\Parameter(
+     *         name="provider",
+     *         in="query",
+     *         required=false,
+     *         example="DataProviderX",
+     *         description="Payment gateway provider"
+     *     ),
+     *     @OA\Parameter(
+     *         name="status",
+     *         in="query",
+     *         required=false,
+     *         example="authorized",
+     *         description="Status of the transaction. Available: authorized, decline and refunded."
+     *     ),
+     *     @OA\Parameter(
+     *         name="balanceMin",
+     *         in="query",
+     *         required=false,
+     *         example="10",
+     *         description="Minimum balance desired. The results will be filtered using this parameter to return the transactions start with the given amount."
+     *     ),
+     *     @OA\Parameter(
+     *         name="balanceMax",
+     *         in="query",
+     *         required=false,
+     *         example="100",
+     *         description="Maximum balance desired. The results will be filtered using this parameter to return the transactions end at the given amount."
+     *     ),
+     *     @OA\Parameter(
+     *         name="currency",
+     *         in="query",
+     *         required=false,
+     *         example="USD",
+     *         description="The currency will be used to filter results."
+     *     ),
      *     @OA\Response(
      *         response=422,
      *         description="Invalid or missing payload",
